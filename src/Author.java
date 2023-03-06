e31baae8d37425a0e5a567b187aae672c4ffb71e
Роман <roma_yakushin@mail.ru>
2023-03-06 14:37:52 +0300

    Выполненное дз

--- src/Author.java
+++ src/Author.java
@@ -1,2 +1,25 @@
 public class Author {
+    private String name;
+    private String soname;
+
+    public Author(String name, String soname) {
+        this.name = name;
+        this.soname = soname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSoname() {
+        return soname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSoname(String soname) {
+        this.soname = soname;
+    }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,36 @@
 public class Book {
+    private String title;
+    private String year;
+    private Author author;
+
+    public Book(String title, String year, Author author) {
+        this.title = title;
+        this.year = year;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
 }
